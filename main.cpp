87d2b2434d6f3d0f507943dfa06dbcd46b107994
JuaniFIUBA <jperezd@fi.uba.ar>
2022-10-31 13:42:42 -0300

    -

--- main.cpp
+++ main.cpp
@@ -1,26 +1,9 @@
-#include <iostream>
-<<<<<<< Updated upstream
-#include "lista.h"
-#include "Animal.h"
-using namespace std;
-
-int main(void)
-{
-    Lista<Animal*> lista_animal;
-    Animal animal1("Juani");
-    Animal *a = &animal1;
-    lista_animal.agregar(a, 0);
-    cout << lista_animal.consulta(0)->mostrar_nombre() << endl;
-=======
 #include <string>
-#include "personalidad.hpp"
-#include "animales.hpp"
 #include "sistema.hpp"
 #include "menu.h"
 
 int main(){
     Menu menu = Menu();
     menu.seleccionar_opcion();
->>>>>>> Stashed changes
     return 0;
 }
