7cd6a376692fc927b3cf6efa35c6632f7a7842cd
LeanFIUBA <lpenaloza@fi.uba.ar>
2022-11-02 20:45:48 -0300

    asf

--- La_reserva_soñada/perro.cpp
+++ La_reserva_soñada/perro.cpp
@@ -1,6 +1,6 @@
 #include "perro.h"
 
-Perro::Perro(string nombre, int edad, string especie, int tamanio, Personalidad *personalidad) : Animal(nombre, edad, especie, personalidad){
+Perro::Perro(string nombre, int edad, string especie, int tamanio, Personalidad *personalidad) : Animal(nombre, edad, tamanio, especie, personalidad){
     this->tamanio = tamanio;
     this->personalidad->modificar_bases(crecimiento_hambre, decrecimiento_higiene);
 }
